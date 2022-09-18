cf43f1bbfe32dcfffc5c669ff4182b9901c18462
Traceur0 <ghost01ghost@gmail.com>
2022-09-18 23:57:31 +0900

    apply formatter 'autopep8'

--- remoteok.py
+++ remoteok.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup as BS
 import requests
 
+
 def scraping_remoteok(term):
     url = f"https://remoteok.com/remote-{term}-jobs"
     request = requests.get(url, headers={"User-Agent": "Kimchi"})
@@ -8,20 +9,24 @@ def scraping_remoteok(term):
         soup = BS(request.text, "html.parser")
         print("Scraping "+term.upper()+f" job info in '{url}'\n")
         jobs = []
-        tbody = soup.select('tr.job')   
+        tbody = soup.select('tr.job')
         for item in tbody:
-            res_company = item.select_one('tr > td.company.position.company_and_position > span.companyLink > h3').text.strip('\n ')
-            res_title = item.select_one('tr > td.company.position.company_and_position > a > h2').text.strip('\n')
+            res_company = item.select_one(
+                'tr > td.company.position.company_and_position > span.companyLink > h3').text.strip('\n ')
+            res_title = item.select_one(
+                'tr > td.company.position.company_and_position > a > h2').text.strip('\n')
             try:
-                res_verified = item.select_one('tr > td.company.position.company_and_position > span.verified.tooltip').text
+                res_verified = item.select_one(
+                    'tr > td.company.position.company_and_position > span.verified.tooltip').text
             except:
                 res_verified = ""
-            res_location = [] # DEFAULT
-            res_salary = "" # DEFAULT
-            res_rule = "" # DEFAULT
+            res_location = []  # DEFAULT
+            res_salary = ""  # DEFAULT
+            res_rule = ""  # DEFAULT
         for idx in range(4):
             try:
-                loc = item.select('tr > td.company.position.company_and_position > div.location')[idx].text
+                loc = item.select(
+                    'tr > td.company.position.company_and_position > div.location')[idx].text
                 if "💰" in loc:
                     res_salary = loc
                 elif "⏰" in loc:
@@ -31,15 +36,18 @@ def scraping_remoteok(term):
             except:
                 break
             res_tag = []
-        for idx in range(0,3):
+        for idx in range(0, 3):
             try:
-                res_tag_child = item.select(f'tr > td.tags > a > div > h3')[idx].text.strip('\n ')
+                res_tag_child = item.select(f'tr > td.tags > a > div > h3')[
+                    idx].text.strip('\n ')
                 res_tag.append(res_tag_child)
             except:
                 pass
-            res_elapsed_time = item.select_one('tr > td.time > time').text.strip('\n ')
+            res_elapsed_time = item.select_one(
+                'tr > td.time > time').text.strip('\n ')
             try:
-                res_closed = item.select_one('tr > td.company.position.company_and_position > span.closed').text
+                res_closed = item.select_one(
+                    'tr > td.company.position.company_and_position > span.closed').text
             except:
                 res_closed = ""
         try:
--- weworkremotely.py
+++ weworkremotely.py
@@ -17,14 +17,15 @@ def link_mkr(element):
     return lower
 '''
 
+
 def scraping_wwr(term):
     url = f"https://weworkremotely.com/remote-jobs/search?utf8=✓&term={term}"
     request = requests.get(url, headers={"User-Agent": "Kimchi"})
     if request.status_code == 200:
         soup = BeautifulSoup(request.text, "html.parser")
-        # write your ✨magical✨ code here
         str_term = term
-        print("Scraping "+str_term.upper()+f" job info in 'https://weworkremotely.com'\n")
+        print("Scraping "+str_term.upper() +
+              f" job info in 'https://weworkremotely.com'\n")
         jobs = []
         job_code = [2, 17, 18, 4]
         """
@@ -36,33 +37,40 @@ def scraping_wwr(term):
         for code in job_code:
             job_li = soup.select(f'#category-{code} > article > ul > li')
             try:
-                job_li.pop() # for the function of the job_li list, last element is printed twice. this is resoluation
+                job_li.pop()  # for the function of the job_li list, last element is printed twice. this is resoluation
             except:
                 pass
             print(code)
             for li in job_li:
                 try:
-                    res_company = li.select_one(f'#category-{code} > article > ul > li > a > span.company').text
-                    res_title = li.select_one(f'#category-{code} > article > ul > li > a > span.title').text
-                    res_jobtype = li.select(f'#category-{code} > article > ul > li > a > span.company')[1].text
-                    res_location = li.select_one(f'#category-{code} > article > ul > li > a > span.region.company').text
+                    res_company = li.select_one(
+                        f'#category-{code} > article > ul > li > a > span.company').text
+                    res_title = li.select_one(
+                        f'#category-{code} > article > ul > li > a > span.title').text
+                    res_jobtype = li.select(
+                        f'#category-{code} > article > ul > li > a > span.company')[1].text
+                    res_location = li.select_one(
+                        f'#category-{code} > article > ul > li > a > span.region.company').text
                     res_elapsed_time = ""
                     try:
-                        res_elapsed_time = li.select_one(f'#category-{code} > article > ul > li > a > span.date > time').text
+                        res_elapsed_time = li.select_one(
+                            f'#category-{code} > article > ul > li > a > span.date > time').text
                     except:
                         if res_elapsed_time == None:
                             res_elapsed_time = "Recent"
-                    res_link_partial = li.select(f'#category-{code} > article > ul > li > a')[0]['href']
+                    res_link_partial = li.select(
+                        f'#category-{code} > article > ul > li > a')[0]['href']
                 except:
                     try:
-                        checking_err = li.select_one(f'#category-{code} > article > ul > li > a > span.title').text
+                        checking_err = li.select_one(
+                            f'#category-{code} > article > ul > li > a > span.title').text
                     except:
                         pass
-                # company_trimmed = link_mkr(res_company) 
+                # company_trimmed = link_mkr(res_company)
                 # title_trimmed = link_mkr(res_title)
                 # apply_link = company_trimmed + "-" + title_trimmed
                 # print(apply_link)
-            
+
                 jobset = {
                     'title': res_title,
                     'company': res_company,
@@ -73,6 +81,6 @@ def scraping_wwr(term):
                 }
                 print(jobset)
                 # jobs.append(jobset)
-            
+
     else:
         print("Can't get jobs.")
