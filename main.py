f0ce363bacf8ce493272c18f8d9350f955acf940
Traceur0 <ghost01ghost@gmail.com>
2022-09-19 17:56:06 +0900

    making into csv file

--- main.py
+++ main.py
@@ -3,9 +3,55 @@ from weworkremotely import *
 
 
 # scraping_remoteok("rust")
-# scraping_remoteok("golang")
-# scraping_remoteok("python")
-# scraping_remoteok("react")
+# scraping_wwr("react")
+
+def transmitter():
+    return input("type what you search for: ")
 
 
-scraping_wwr("react")
+keyword = transmitter()
+
+remoteok = scraping_remoteok(keyword)
+# wwr = scraping_wwr(keyword)
+
+jobset_remoteok = {
+                'title': res_title.string,
+                'company': res_company.string,
+                'location': res_location.string,
+                'jobtype': res_jobtype.string,
+                'elapsed_time': res_elapsed_time.string,
+                'apply_link': link,
+                'salary': res_salary,
+                'tag': res_tag,
+                'verified': res_verified,
+                'closed': res_closed
+            }
+jobset_wwr = {
+                    'title': res_title,
+                    'company': res_company,
+                    'location': res_location,
+                    'jobtype': res_jobtype,
+                    'elapsed_time': res_elapsed_time,
+                    'apply_link': "https://weworkremotely.com" + res_link_partial
+                }
+
+# Making file for RemoteOk.com
+file = open(f"{keyword}.csv", "w", encoding = "utf-8")
+file.write("title, company, location, jobtype, elapsed_time, apply_link, salary, tag, verified, closed")
+
+for unit in remoteok:
+    file.write(f"""{jobset_remoteok['title']}, 
+                {jobset_remoteok['company']}, 
+                {jobset_remoteok['location']}, 
+                {jobset_remoteok['jobtype']}, 
+                {jobset_remoteok['elapsed_time']},
+                {jobset_remoteok['apply_link']},
+                {jobset_remoteok['salary']},
+                {jobset_remoteok['tag']},
+                {jobset_remoteok['verified']},
+                {jobset_remoteok['closed']}\n
+                """)
+    file.close()
+
+# Making file for WeWorkRemotely.com
+file = open()
--- weworkremotely.py
+++ weworkremotely.py
@@ -1,21 +1,24 @@
-from bs4 import BeautifulSoup
 import requests
-import re
+from bs4 import BeautifulSoup
 
-'''
-def link_mkr(element):
-    ## applyLink는 company 스트링과 title 스트링 사이에 -(dash)를 넣고 
-    ##   공백을 모두 제거 후 -로 변환, 특수문자 제거(/, (, )등 ), 알파벳 전부 소문자화 시킨 값과 같음
+# import re
 
-    de_indented = element.replace(' ', '-').replace('/', '-'). replace('.', '-') # 공백을 -로 변경
-    simplified = re.sub('[^A-z0-9-]+', '', de_indented) # 특수문자 제거
-    lower = simplified.lower() # 소문자화
-    if "---"  in lower:
-        lower = lower.replace('---', '-')
-    if "--" in lower:
-        lower = lower.replace('--', '-')
-    return lower
-'''
+
+def code_replacer(code):
+    if code == 2:
+        print("Category: Full-Stack")
+    elif code == 17:
+        print("Category: Front-End")
+    elif code == 18:
+        print("Category: Back-End")
+    elif code == 4:
+        print("Category: All Other Remote Jobs")
+    """
+        'Full-Stack': 2,
+        'Front-End' : 17,
+        'Back-End': 18,
+        'All Other Remote Jobs': 4
+        """    
 
 
 def scraping_wwr(term):
@@ -24,53 +27,39 @@ def scraping_wwr(term):
     if request.status_code == 200:
         soup = BeautifulSoup(request.text, "html.parser")
         str_term = term
-        print("Scraping "+str_term.upper() +
-              f" job info in 'https://weworkremotely.com'\n")
+        print("Scraping "+str_term.upper()+f" job info in 'https://weworkremotely.com'\n")
         jobs = []
         job_code = [2, 17, 18, 4]
-        """
-        'Full-Stack': 2,
-        'Front-End' : 17,
-        'Back-End': 18,
-        'All Other Remote Jobs': 4
-        """
         for code in job_code:
             job_li = soup.select(f'#category-{code} > article > ul > li')
             try:
-                job_li.pop()  # for the function of the job_li list, last element is printed twice. this is resoluation
+                job_li.pop() # for the function of the job_li list, last element is printed twice. this is resoluation
             except:
                 pass
-            print(code)
+            code_replacer(code)
             for li in job_li:
                 try:
-                    res_company = li.select_one(
-                        f'#category-{code} > article > ul > li > a > span.company').text
-                    res_title = li.select_one(
-                        f'#category-{code} > article > ul > li > a > span.title').text
-                    res_jobtype = li.select(
-                        f'#category-{code} > article > ul > li > a > span.company')[1].text
-                    res_location = li.select_one(
-                        f'#category-{code} > article > ul > li > a > span.region.company').text
+                    res_company = li.select_one(f'#category-{code} > article > ul > li > a > span.company').text
+                    res_title = li.select_one(f'#category-{code} > article > ul > li > a > span.title').text
+                    res_jobtype = li.select(f'#category-{code} > article > ul > li > a > span.company')[1].text
+                    res_location = li.select_one(f'#category-{code} > article > ul > li > a > span.region.company').text
+
+                    # Default value
                     res_elapsed_time = ""
                     try:
-                        res_elapsed_time = li.select_one(
-                            f'#category-{code} > article > ul > li > a > span.date > time').text
+                        res_elapsed_time = li.select_one(f'#category-{code} > article > ul > li > a > span.date > time').text
                     except:
                         if res_elapsed_time == None:
                             res_elapsed_time = "Recent"
-                    res_link_partial = li.select(
-                        f'#category-{code} > article > ul > li > a')[0]['href']
+
+                    res_link_partial = li.select(f'#category-{code} > article > ul > li > a')[0]['href']
+
                 except:
                     try:
-                        checking_err = li.select_one(
-                            f'#category-{code} > article > ul > li > a > span.title').text
+                        checking_err = li.select_one(f'#category-{code} > article > ul > li > a > span.title').text
                     except:
                         pass
-                # company_trimmed = link_mkr(res_company)
-                # title_trimmed = link_mkr(res_title)
-                # apply_link = company_trimmed + "-" + title_trimmed
-                # print(apply_link)
-
+            
                 jobset = {
                     'title': res_title,
                     'company': res_company,
@@ -79,8 +68,8 @@ def scraping_wwr(term):
                     'elapsed_time': res_elapsed_time,
                     'apply_link': "https://weworkremotely.com" + res_link_partial
                 }
-                print(jobset)
-                # jobs.append(jobset)
+                jobs.append(jobset)
 
+            return jobs       
     else:
         print("Can't get jobs.")
