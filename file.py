f983505bfd07fb3cdfa7ba2ee22497f26e11c051
Traceur0 <ghost01ghost@gmail.com>
2022-10-05 15:16:59 +0900

    some optimization & export to csv button added

--- file.py
+++ file.py
@@ -1,34 +1,24 @@
+from time import time, localtime, strftime
+
+
+
+init = strftime('%y%m%d%H%M', localtime(time()))
+
 def mk_remoteok_into_csv(keyword, remoteok):
     # Making file for RemoteOk.com
-    file = open(f"{keyword}.csv", "w", encoding = "utf-8")
-    file.write("title, company, location, jobtype, elapsed_time, apply_link, salary, tag, verified, closed")
+    file = open(f"remoteok_{keyword}_{init}.csv", "wt", encoding="utf-8-sig")
+    file.write("title,company,location,jobtype,elapsed_time,apply_linksalary,tag,verified,closed\n")
 
     for jobset in remoteok:
-        file.write(f"""{jobset['title']}, 
-                    {jobset['company']}, 
-                    {jobset['location']}, 
-                    {jobset['jobtype']}, 
-                    {jobset['elapsed_time']},
-                    {jobset['apply_link']},
-                    {jobset['salary']},
-                    {jobset['tag']},
-                    {jobset['verified']},
-                    {jobset['closed']}\n 
-                    """)
-        file.close()
+        file.write(f"{jobset['title']}, {jobset['company']}, {jobset['location']}, {jobset['jobtype']}, {jobset['elapsed_time']},{jobset['apply_link']},{jobset['salary']},{jobset['tag']},{jobset['verified']},{jobset['closed']}\n ")
+    file.close()
 
 
 def mk_wwr_into_csv(keyword, wwr):
     # Making file for WeWorkRemotely.com
-    file = open(f"{keyword}.csv", "w", encoding = "utf-8")
+    file = open(f"weworkremotely_{keyword}_{init}.csv", "wt", encoding = "utf-8-sig")
     file.write("title, company, location, jobtype, elapsed_time, apply_link\n")
 
     for jobset in wwr:
-        file.write(f"""{jobset['title']},
-                    {jobset['company']},
-                    {jobset['location']},
-                    {jobset['jobtype']},
-                    {jobset['elapsed_time']},
-                    {jobset['apply_link']}\n
-                    """)
-        file.close()
+        file.write(f"{jobset['title']},{jobset['company']},{jobset['location']},{jobset['jobtype']},{jobset['elapsed_time']},{jobset['apply_link']}\n")
+    file.close()
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from remoteok import *
 from weworkremotely import *
 from file import *
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, send_file
 
 
 app = Flask("JobScrapper")
+
+database = {}
 
 @app.route("/")
 def main():
@@ -13,8 +15,24 @@ def main():
 @app.route("/search")
 def search():
     keyword = request.args.get("keyword")
-    remoteok = scraping_remoteok(keyword)
+    if keyword == None:
+        return redirect("/")
+    if keyword in database:
+        remoteok = database[keyword]
+    else:
+        remoteok = scraping_remoteok(keyword)
+        database[keyword] = remoteok
     return render_template("result.html", keyword=keyword, remoteok=remoteok)
+
+@app.route("/export")
+def export():
+    keyword = request.args.get("keyword")
+    if keyword == None:
+        return redirect("/")
+    if keyword not in database:
+        return redirect(f"/search?keyword={keyword}")
+    mk_remoteok_into_csv(keyword, database[keyword])
+    return send_file(f"{keyword}.csv", as_attachment=True)
 
 app.run("127.0.0.1")
 
